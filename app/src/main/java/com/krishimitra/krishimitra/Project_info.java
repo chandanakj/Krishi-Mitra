405b93f922f75ffb1e795885b1e88454b6e79725
abhayraj95 <abhayraj_27@yahoo.co.in>
2015-09-28 23:22:32 +0530

    button link to donation

--- app/src/main/java/com/krishimitra/krishimitra/Project_info.java
+++ app/src/main/java/com/krishimitra/krishimitra/Project_info.java
@@ -1,14 +1,15 @@
 package com.krishimitra.krishimitra;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 
-public class Project_info extends ActionBarActivity {
+public class Project_info extends ActionBarActivity implements View.OnClickListener {
 
     TextView tv6;
     String str = "<u>underline</u>";
@@ -43,5 +44,11 @@ public class Project_info extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent4 = new Intent(Project_info.this, donation.class);
+        startActivity(intent4);
     }
 }
--- app/src/main/java/com/krishimitra/krishimitra/donation.java
+++ app/src/main/java/com/krishimitra/krishimitra/donation.java
@@ -1,7 +1,8 @@
 package com.krishimitra.krishimitra;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,8 @@ public class donation extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donation);
+
+        Intent intent4 = getIntent();
     }
 
 
