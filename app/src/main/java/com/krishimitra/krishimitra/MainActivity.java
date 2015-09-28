64814abcc51b42831337d2176beb3b3481d910dc
abhayraj95 <abhayraj_27@yahoo.co.in>
2015-09-28 22:44:04 +0530

    reversion of errors

--- app/src/main/java/com/krishimitra/krishimitra/MainActivity.java
+++ app/src/main/java/com/krishimitra/krishimitra/MainActivity.java
@@ -1,13 +1,13 @@
 package com.krishimitra.krishimitra;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
     Button donor,farmer;
 
 
