7f185612ab3028493cad861572c7027fc6f1f31a
CHANDANA <channu1995@gmail.com>
2015-09-29 02:56:07 +0530

    manifest changes

--- app/src/main/java/com/krishimitra/krishimitra/CardView.java
+++ app/src/main/java/com/krishimitra/krishimitra/CardView.java
@@ -3,8 +3,6 @@ package com.krishimitra.krishimitra;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.dexafree.materialList.card.Card;
